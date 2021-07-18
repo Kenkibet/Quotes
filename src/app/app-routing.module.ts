929a4e70176ac2533741eee3024db3c02b7c4b40
kenkibet <kenkibet@gmail.com>
2021-07-18 11:19:34 -0700

    Added home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AddQuotesComponent } from './components/add-quotes/add-quotes.component'
+import { ListQuotesComponent } from './components/list-quotes/list-quotes.component'
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'add',
+    redirectTo: 'home',
   },
 
+  {
+    path:'home', component: ListQuotesComponent
+  },
   {
     path:'add', component: AddQuotesComponent
   }
