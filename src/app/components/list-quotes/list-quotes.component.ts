2ccba848aa52e5f343c9d2ed461eda010df3c846
kenkibet <kenkibet@gmail.com>
2021-07-18 11:24:10 -0700

    display quotes on home route

--- src/app/components/list-quotes/list-quotes.component.ts
+++ src/app/components/list-quotes/list-quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/interfaces/quote';
+import { QuotesService } from 'src/app/services/quotes.service';
 
 @Component({
   selector: 'app-list-quotes',
@@ -6,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
+
 export class ListQuotesComponent implements OnInit {
 
-  constructor() { }
+  quotes:Quote[]
+
+  constructor(private qouteService: QuotesService) { }
 
   ngOnInit(): void {
+    this.quotes = this.qouteService.getAllQuotes();
   }
 
 }
