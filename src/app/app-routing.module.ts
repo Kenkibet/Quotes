883b09adedc827bf85dd17ea816f862d1270c935
kenkibet <kenkibet@gmail.com>
2021-07-18 10:55:21 -0700

    added add quote form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AddQuotesComponent } from './components/add-quotes/add-quotes.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'add',
+  },
+
+  {
+    path:'add', component: AddQuotesComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbaComponent } from './components/navba/navba.component';
+import { AddQuotesComponent } from './components/add-quotes/add-quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbaComponent
+    NavbaComponent,
+    AddQuotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
